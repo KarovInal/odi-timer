6e672dee5f65261ee13f7d214c67431dc0986f50
Zhumabaev <nadir.zhumabaev@yandex.ru>
2020-06-24 21:05:51 +0300

    feat(footer): Added `Footer` to page

--- src/modules/page-module/page.tsx
+++ src/modules/page-module/page.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
-import { Header, IHeader } from "../header-module";
+import { Header, IHeader } from '../header-module';
+import { Footer } from '../footer-module';
 
 export interface IPage {
     children: React.ReactNode;
     headerProps: IHeader;
 }
 
-export const Page = (props: IPage) => {
-    return (
-        <div>
-            <Header {...props.headerProps} />
-            { props.children }
-            {/*<Footer/>*/}
-        </div>
-    );
+export const Page = (props: IPage): React.ReactElement => {
+  return (
+    <div>
+      <Header {...props.headerProps} />
+      { props.children }
+      <Footer />
+    </div>
+  );
 };
