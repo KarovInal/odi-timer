945c48a2c9fc2a6ca38cfb90ee090f81b9ef1a65
i.karov <i.karov@alibaba-inc.com>
2020-06-27 16:18:06 +0300

    Task items component

---
+++ src/modules/tasks-module/tasks-types.ts
@@ -0,0 +1,12 @@
+export enum EControl {
+    play = 'play',
+    pause = 'pause',
+    replay = 'replay',
+    finished = 'finished',
+}
+
+export enum EStatus {
+    great ='great',
+    normal ='normal',
+    bad ='bad',
+}
