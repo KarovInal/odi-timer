dbb93d07e9b156e94fb08e2c00f959dfb836e687
i.karov <i.karov@alibaba-inc.com>
2020-07-12 01:20:06 +0300

    Fixed Progress component Fixed long title text

--- src/modules/statistics/bottom-block/bottom-block.tsx
+++ src/modules/statistics/bottom-block/bottom-block.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import { isEmpty } from 'lodash/fp';
+import { EDIT_TASK } from '@/constants';
 import { Link } from 'react-router-dom';
 import { msToHms } from '@/utils/ms-to-hms';
 import { ITaskItem } from '@/modules/tasks-module';
+import { HelperText } from '@/components/helper-text';
 import './bottom-block.css';
 
 type TBottomBlockProps = {
@@ -10,21 +13,22 @@ type TBottomBlockProps = {
 
 export const BottomBlock = React.memo(({
   finishedTasks
-}: TBottomBlockProps) => (
-  <div>
-    {finishedTasks.map(({
-      id,
-      title,
-      finalTime,
-      status,
-    }) => (
-      <Link to="#" className="bottom-block-item" key={id}>
+}: TBottomBlockProps) => {
+  if (isEmpty(finishedTasks)) {
+    return <HelperText>
+      statistics are not available because no tasks have been added
+    </HelperText>;
+  }
+
+  return <div>
+    {finishedTasks.map(({id, title, finalTime, status}) => (
+      <Link to={`${EDIT_TASK}/${id}`} className="bottom-block-item" key={id}>
         <div className="bottom-block-item-label">
           <span className={`bottom-block-item-dot bottom-block-item-dot-${status}`} />
-          <span>{title}</span>
+          <span className='bottom-block-item-title'>{title}</span>
         </div>
         <span className={`bottom-block-item-${status}`}>{msToHms(finalTime)}</span>
       </Link>
     ))}
-  </div>
-));
+  </div>;
+});
--- src/modules/statistics/statistics.tsx
+++ src/modules/statistics/statistics.tsx
@@ -1,23 +1,12 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { isEmpty } from 'lodash/fp';
 import { ITaskItem } from '@/modules/tasks-module';
-import { HelperText } from '@/components/helper-text';
 import { getFinishedTasks } from './statistics-selectors';
 import { TopBlock } from '@/modules/statistics/top-block/tob-block';
 import { BottomBlock } from '@/modules/statistics/bottom-block';
 
-type TStatisticsProps = {
-};
-
-export const Statistics = React.memo<TStatisticsProps>(() => {
+export const Statistics = React.memo(() => {
   const finishedTasks: ITaskItem[] = useSelector(getFinishedTasks);
-
-  if (isEmpty(finishedTasks)) {
-    return <HelperText>
-      statistics are not available because no tasks have been added
-    </HelperText>;
-  }
 
   return (
     <React.Fragment>
